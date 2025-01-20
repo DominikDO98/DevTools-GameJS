b8fc47fd8991af20671b8ca21441ecdaf3aae223
DominikDO98 <dominiklas13@wp.pl>
2025-01-20 21:15:57 +0100

    Feat: Integration test for mongo/connection added

---
+++ src/__tests__/integration/mongo/connection.test.ts
@@ -0,0 +1,50 @@
+import { MongoConnection } from "../../../mongo/connection";
+import mongoose from "mongoose";
+
+describe("MongoConnection", () => {
+  const mongoConn = new MongoConnection();
+  beforeAll(() => {
+    process.env.MONGODB_URI = "someuri";
+    process.env.MONGODB_DB = "somedb";
+    jest
+      .spyOn(mongoose, "connect")
+      .mockImplementation((uri: string, options?: mongoose.ConnectOptions) => {
+        return new Promise((resolve) => {
+          setTimeout(() => {
+            console.log(uri);
+            console.log(options);
+            resolve(new mongoose.Mongoose());
+          }, 200);
+        });
+      });
+    jest.spyOn(mongoose, "disconnect").mockImplementation(() => {
+      return new Promise((resolve) => {
+        setTimeout(() => {
+          resolve();
+        });
+      });
+    });
+    jest.spyOn(mongoose.connection, "useDb");
+  });
+  afterAll(() => {
+    jest.clearAllMocks();
+  });
+
+  describe("Pass", () => {
+    it("MongoConnect calls connect", async () => {
+      await mongoConn.init().then(() => {
+        expect(mongoose.connect).toHaveBeenCalledTimes(1);
+        expect(mongoose.connect).toHaveBeenCalledWith(process.env.MONGODB_URI);
+        expect(mongoose.connection.useDb).toHaveBeenCalledTimes(1);
+        expect(mongoose.connection.useDb).toHaveBeenCalledWith(
+          process.env.MONGODB_DB
+        );
+        expect(mongoConn.connection).toBeDefined();
+      });
+    });
+    it("MongoConnect calls disconnect", async () => {
+      mongoConn.disconnect();
+      expect(mongoose.disconnect).toHaveBeenCalledTimes(1);
+    });
+  });
+});
